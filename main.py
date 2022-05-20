8c6319e4974772dca8d8c24c46eb5da453311709
Foram Patel <105926611+foram2911@users.noreply.github.com>
2022-05-20 18:49:37 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+
+import cv2
+capture = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+cv2.namedWindow("Window")
+
+while True:
+    ret, frame = capture.read()
+    # converting  it to grascale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.5, 5)
+    # print faces
+    for (x, y, w, h) in faces:
+      cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    cv2.imshow("Window",frame)
+    #This breaks on 'q' key
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
